e6e45c0ce8112006f16351b223fe4403966b2ea6
abdulsalam shahlol <abdulsalamshahlol@gmail.com>
2019-11-25 15:16:49 +0100

    HoneyMakerBee class defined and complete

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,22 @@
 var HoneyMakerBee = function() {
+ // Invokes the constructor from Bee Class and inherits properties
+	Bee.call(this);
+	this.age = 10;
+	this.job = 'make honey';
+	this.honeyPot = 0;
 };
 
+//Inherit the methods from the parent class
+HoneyMakerBee.prototype = Object.create(Grub.prototype);
+
+// Class functions
+HoneyMakerBee.prototype.makeHoney = function() {
+	this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+	this.honeyPot--;
+};
+
+// Constructor method that institates Object from class
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
