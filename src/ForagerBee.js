74b631917bc24482a30d0d366af0bc7b938f4c2b
abdulsalam shahlol <abdulsalamshahlol@gmail.com>
2019-11-25 15:25:10 +0100

    ForagerBee class defined and complete

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,21 @@
 var ForagerBee = function() {
+ // Invokes the constructor from Bee Class and inherits properties
+	Bee.call(this);
+	this.age = 10;
+	this.job = 'find pollen';
+	this.canFly = true;
+	this.treasureChest = new Array();
 };
 
+// Inherit the methods from the parent class
+ForagerBee.prototype = Object.create(Grub.prototype);
+
+// Class functions
+ForagerBee.prototype.forage = function(forage) {
+	if(forage){
+		this.treasureChest.push(forage);
+	}
+}
+
+// Constructor method that institates Object from class
+ForagerBee.prototype.constructor = ForagerBee;
